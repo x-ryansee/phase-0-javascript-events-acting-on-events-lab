a803dc1f54af98fe28bb8b9a5d2eb64a4624e761
Ryan See <ryanjosephsee@gmail.com>
2022-11-04 09:02:36 -0700

    commit tree c5212219b97118e725d037c4293961266f92d2db

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+  
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Arrowight") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left + 1}px`;
+    }
+  });
+  
